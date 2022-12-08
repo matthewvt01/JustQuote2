ec643f99283fc38f72aa3ef1cd3143670dd1f6a3
matthewvt01 <matthewvincentius01@gmail.com>
2022-12-08 13:05:20 +0700

    JustQuote API, Adapter, RetroServer

---
+++ app/src/main/java/com/si5b/just_quote/Activity/MainActivity.java
@@ -0,0 +1,26 @@
+    package com.si5b.just_quote.Activity;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import android.os.Bundle;
+import android.widget.ProgressBar;
+
+import com.si5b.just_quote.R;
+
+    public class MainActivity extends AppCompatActivity {
+        private RecyclerView rvQuote;
+        private ProgressBar pbQuote;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        rvQuote = findViewById(R.id.rv_quote);
+        pbQuote = findViewById(R.id.pb_quote);
+
+        rvQuote.setLayoutManager(new LinearLayoutManager(this));
+    }
+}
---
+++ app/src/main/java/com/si5b/just_quote/Adapter/AdapterQuote.java
@@ -0,0 +1,56 @@
+package com.si5b.just_quote.Adapter;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.si5b.just_quote.Model.QuoteModel;
+import com.si5b.just_quote.R;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class AdapterQuote extends RecyclerView.Adapter<AdapterQuote.ViewHolder>{
+    private List<QuoteModel> listQuote = new ArrayList<>();
+    private Context ctx;
+
+    public AdapterQuote(List<QuoteModel> listQuote, Context ctx) {
+        this.listQuote = listQuote;
+        this.ctx = ctx;
+    }
+
+    @NonNull
+    @Override
+    public ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View V = LayoutInflater.from(ctx).inflate(R.layout.item_quote, parent, false);
+        return new ViewHolder(V);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull ViewHolder holder, int position) {
+        QuoteModel QM = listQuote.get(position);
+        holder.tvText.setText(QM.getText());
+        holder.tvAuthor.setText(QM.getAuthor());
+
+    }
+
+    @Override
+    public int getItemCount() {
+        return listQuote.size();
+    }
+
+    public class ViewHolder extends RecyclerView.ViewHolder{
+        TextView tvText, tvAuthor;
+
+        public ViewHolder(@NonNull View itemView) {
+            super(itemView);
+            tvText = itemView.findViewById(R.id.tv_text);
+            tvAuthor = itemView.findViewById(R.id.tv_author);
+        }
+    }
+}
